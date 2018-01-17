1850eebf3e4743d8a1ffc301605051b130291847
anmolduainter <anmolduainter@gmail.com>
2018-01-17 22:03:57 +0530

    .

--- app/src/main/java/com/example/anmol/childmonitor/MainActivity.kt
+++ app/src/main/java/com/example/anmol/childmonitor/MainActivity.kt
@@ -2,11 +2,32 @@ package com.example.anmol.childmonitor
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    var phoneNumber: EditText? = null;
+    var btn: Button? = null;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        phoneNumber = findViewById<EditText>(R.id.editText)
+        btn = findViewById<Button>(R.id.button)
+
+        btn?.setOnClickListener({
+            Toast.makeText(this,"Button Clicked" , Toast.LENGTH_LONG).show()
+
+            //Taking Phone Number
+            val ph = phoneNumber?.text;
+
+            //Sending the SMS
+
+            //Receiving the SMS
+
+        })
     }
 }
