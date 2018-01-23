c75e2175eb2ccb975be4a758620f576b69f7a07e
anmolduainter <anmolduainter@gmail.com>
2018-01-23 12:14:25 +0530

    .

--- app/src/main/java/com/example/anmol/childmonitor/MainActivity.kt
+++ app/src/main/java/com/example/anmol/childmonitor/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.anmol.childmonitor
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -10,6 +15,8 @@ class MainActivity : AppCompatActivity() {
 
     var phoneNumber: EditText? = null;
     var btn: Button? = null;
+
+    var broadcast:BroadcastReceiver? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +34,35 @@ class MainActivity : AppCompatActivity() {
             //Sending the SMS
 
             //Receiving the SMS
-
         })
+
+        broadcast = object : BroadcastReceiver(){
+            override fun onReceive(p0: Context?, p1: Intent?) {
+                var senderName : String = p1!!.getStringExtra("phoneNumber")
+                var message: String = p1!!.getStringExtra("message")
+                println(message)
+                sendOTP(message)
+            }
+        }
+
+    }
+
+    // OTP VERIFICATION
+    fun sendOTP(message:String){
+        //FOR NOW 1234
+        if (message.equals("1234")){
+            println("OTP VERIFIED")
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val intentFilter : IntentFilter = IntentFilter("smsReceiver")
+        registerReceiver( broadcast , intentFilter)
+    }
+
+    override fun onPause() {
+        unregisterReceiver(broadcast)
+        super.onPause()
     }
 }
